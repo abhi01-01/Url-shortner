8255444827b2b6c180605387418f4eef71f21391
Abhishek Singh <88226074+abhi01-01@users.noreply.github.com>
2022-09-20 23:02:03 +0530

    Bug fixed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrl')
 const app = express()
 
-mongoose.connect('process.env.MONGO_URL || "mongodb://localhost/urlShortener"',{
+mongoose.connect(process.env.MONGO_URL || "mongodb://localhost/urlShortener",{
     useNewUrlParser: true, useUnifiedTopology:true
 })
 
